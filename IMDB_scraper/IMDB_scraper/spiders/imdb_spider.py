7315480ca474bb2517fd3a9c614b23b3d6130a22
shizuchanw <florawang0611@gmail.com>
2021-10-16 20:44:12 -0700

    Create imdb_spider.py

---
+++ IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
@@ -0,0 +1,28 @@
+import scrapy
+
+class ImdbSpider(scrapy.Spider):
+    name = 'imdb_spider'
+    allowed_domains = ["www.imdb.com"]
+    start_urls = ['https://www.imdb.com/title/tt0816692/']
+
+    def parse(self, response):
+        credit_url = response.url + "fullcredits/"
+        yield scrapy.Request(credit_url, callback = self.parse_full_credits)
+
+    def parse_full_credits(self, response):
+        url_boxes = response.css("td.primary_photo + td").css("a")
+        actor_urls = ["https://www.imdb.com" + url_box.attrib["href"] for url_box in url_boxes]
+
+        for actor_url in actor_urls: 
+            yield scrapy.Request(actor_url, callback = self.parse_actor_page)
+
+    def parse_actor_page(self, response):
+        actor_name = response.css("span.itemprop::text")[0].get()
+        movie_box = response.css("div.filmo-category-section")[0]
+        movie_or_TV_name = movie_box.css("b a::text").getall()
+        
+        yield {
+            "actor" : actor_name, 
+            "movie_or_TV_name" : movie_or_TV_name
+        }
+
